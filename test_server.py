43f79395da26877833d51a74f7c3eb0806f5cd52
Bimgor_Ayk <haykooo3@gmail.com>
2025-01-14 15:50:08 +0300

    commit start 3

---
+++ test_server.py
@@ -0,0 +1,36 @@
+import asyncio
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+TOKEN = '7740369738:AAF8T6rAlItSurtIQN4I9oCHrs-A8yOIW9A'
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        print('Бот запущен!!!')
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
